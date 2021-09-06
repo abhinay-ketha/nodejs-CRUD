cd3157955d913ee76b71b639839645f4869f9180
abhinay-ketha <abhinay.ketha@gmail.com>
2021-09-06 13:47:45 +0530

    cors integration

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv';
 dotenv.config();
 import morgan from 'morgan';
+import cors from 'cors';
 import user  from './routes/user.js';
 import * as database from './db/database.js'
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 database.initialize();
 
 app.use(morgan("dev"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/user', user);
